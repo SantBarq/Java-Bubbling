37fcea67899cf3a94ec42b838febe63ba15ea108
SantBarq <santiagobarquero1@gmail.com>
2021-04-26 22:37:30 -0600

    listo

--- script.js
+++ script.js
@@ -6,10 +6,31 @@ btn.onclick = function() {
   videoBox.setAttribute('class','showing');
 }
 
-videoBox.onclick = function() {
-  videoBox.setAttribute('class','hidden');
-};
+videoBox.addEventListener ('click', function(event) {
+  switch (event.target.id) {
+    case 'play':
+      video.play();
+      break;
 
-video.onclick = function() {
+    case 'pause':
+      video.pause();
+      break;
+
+    case 'stop':
+      video.pause();
+      video.currentTime = 0;
+      break;
+
+    default:
+      videoBox.setAttribute('class','hidden')
+      break;
+  }
+  videoBox.setAttribute('class','hidden');
+  console.log('videoBox.onclick');
+},false);
+
+video.addEventListener('click', function(event) {
+  event.stopPropagation();
   video.play();
-};
+  console.log('video.onclick');
+}, false);
